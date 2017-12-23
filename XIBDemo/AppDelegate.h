65507048f16ab9ed378fdd7e86c7686d1a942ae2
WangXueqi <jkwxq@sina.com>
2017-12-23 10:09:18 +0800

    Initial Commit

---
+++ XIBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XIBDemo
+//
+//  Created by WangXueqi on 2017/12/23.
+//  Copyright © 2017年 JingBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

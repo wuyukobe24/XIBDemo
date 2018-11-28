af6ec867905c6d2179307ad19c20faa06e6d9122
chengqianpeng <chengqianpeng>
2018-11-28 10:43:12 +0800

    删除所有

--- XIBDemo/AppDelegate.m
+++ XIBDemo/AppDelegate.m
@@ -17,9 +17,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    //test
-    //fork111
-    //master
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     MainViewController * main = [[MainViewController alloc]initWithNibName:@"MainViewController" bundle:[NSBundle mainBundle]];
